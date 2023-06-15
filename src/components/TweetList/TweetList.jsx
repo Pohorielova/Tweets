fc8df33f80f24d5d897b36d4e0c804ef1d88e687
Pohorielova <anna.pogorielova@gmail.com>
2023-06-15 20:49:16 +0200

    add tweets

--- src/components/TweetList/TweetList.jsx
+++ src/components/TweetList/TweetList.jsx
@@ -1,15 +1,49 @@
-import { Avatar, Box, Card, Line, List } from "./TweetList.styled";
+import React, { useState } from "react";
+import {
+  Avatar,
+  AvatarThumb,
+  BoxBottom,
+  BoxTop,
+  Btn,
+  Card,
+  Line,
+  List,
+  Title,
+} from "./TweetList.styled";
 
 const TweetList = ({ tweets }) => {
+  const [displayedTweets, setDisplayedTweets] = useState(3); // Стан для відстеження кількості відображених твітів
+
+  const handleLoadMore = () => {
+    setDisplayedTweets(displayedTweets + 3);
+  };
+
   return (
     <List>
-      {tweets.map((tweet) => (
+      {tweets.slice(0, displayedTweets).map((tweet) => (
         <Card key={tweet.id}>
-          <Box />
-          <Line />
-          <Avatar>{/* <img src={tweet.avatar} /> */}</Avatar>
+          <BoxTop />
+          <Line>
+            <AvatarThumb>
+              <Avatar src={tweet.avatar} alt="avatar" />
+            </AvatarThumb>
+          </Line>
+          <BoxBottom>
+            <Title>{tweet.tweets} tweets</Title>
+            <Title>
+              {tweet.followers.toLocaleString("en", {
+                useGrouping: true,
+                minimumFractionDigits: 0,
+              })}{" "}
+              followers
+            </Title>
+            <Btn>Follow</Btn>
+          </BoxBottom>
         </Card>
       ))}
+      {displayedTweets < tweets.length && (
+        <Btn onClick={handleLoadMore}>Load More</Btn>
+      )}
     </List>
   );
 };
--- src/components/TweetList/TweetList.styled.jsx
+++ src/components/TweetList/TweetList.styled.jsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 import cardImage from "C:/Users/atsio/Documents/GitHub/Tweets/src/images/cardImg.png";
+import logo from "C:/Users/atsio/Documents/GitHub/Tweets/src/images/Logo.png";
+import "../../index.css";
 export const List = styled.ul`
   text-decoration: none;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 22px;
 `;
 export const Card = styled.li`
   width: 380px;
@@ -15,17 +20,24 @@ export const Card = styled.li`
   box-shadow: -2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23);
   border-radius: 20px;
 `;
-export const Box = styled.div`
+export const BoxTop = styled.div`
   width: 380px;
   height: 214px;
   background-image: url("${cardImage}");
   background-position: right 36px top 28px;
   background-size: 308px 168px;
   background-repeat: no-repeat;
+  &::before {
+    content: "";
+    transform: translate(20px, 20px);
+    display: block;
+    width: 76px;
+    height: 22px;
+    background-image: url("${logo}");
+    background-repeat: no-repeat;
+  }
 `;
-export const Line = styled.span`
-  display: block;
-  position: relative;
+export const Line = styled.div`
   width: 380px;
   height: 8px;
 
@@ -33,11 +45,8 @@ export const Line = styled.span`
   box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06),
     inset 0px -1.71846px 3.43693px #ae7be3, inset 0px 3.43693px 2.5777px #fbf8ff;
 `;
-export const Avatar = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+export const AvatarThumb = styled.div`
+  transform: translate(150px, -50%);
   width: 80px;
   height: 80px;
   background: #ebd8ff;
@@ -45,4 +54,57 @@ export const Avatar = styled.div`
     inset 0px -2.19582px 4.39163px #ae7be3,
     inset 0px 4.39163px 3.29372px #fbf8ff;
   border-radius: 85.9232px;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  object-fit: contain;
+`;
+
+export const Avatar = styled.img`
+  width: 64px;
+  height: 64px;
+  border-radius: 85.9232px;
+`;
+export const BoxBottom = styled.div`
+  margin-top: 62px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  align-items: center;
+`;
+export const Title = styled.p`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 1.2;
+  text-transform: uppercase;
+
+  color: #ebd8ff;
+`;
+export const Btn = styled.button`
+  display: inline-block;
+  width: 196px;
+  height: 50px;
+  padding: 14px 28px;
+  background: #ebd8ff;
+  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
+  border-radius: 10.3108px;
+  transition: background-color 0.3s ease;
+  color: #373737;
+  text-transform: uppercase;
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 1.22;
+  border: none;
+  margin-top: 10px;
+  &:hover {
+    transform: scale(1.05);
+  }
+  &:active {
+    background: #5cd3a8;
+  }
 `;
